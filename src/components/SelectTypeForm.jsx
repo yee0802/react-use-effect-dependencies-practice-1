8309b31e367f4f1406a0121f0fc6e00e62d9fa6c
Kye <NovusAa12@proton.me>
2023-11-02 12:55:01 +0000

    add onChange func.

--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,11 +1,10 @@
 function SelectTypeForm(props) {
-  // Write code here...
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
-        <option value="">Please select...</option>
+      <select id="type" name="type" onChange={(e) => props.setDataType(e.target.value)}>
+        <option value="" >Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
         <option value="starships">Starships</option>
